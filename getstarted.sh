b7a893203c5fc892267611cee1525cfd79a96a89
Cdaq User <cdaq@jlab.org>
2019-07-20 00:46:54 -0400

    some path not needed anymore

--- getstarted.sh
+++ getstarted.sh
@@ -8,18 +8,6 @@
 # don't forget to go to "Ana", then do: make clean && make
 
 savepwd=$(pwd)
-pwd
-cd ${savepwd}
-ls
-sed -i -e "s%/home/cdaq/vcs2019/hallc_replay_vcs/%${savepwd}/%" run_ana_only.csh
-sed -i -e "s%/home/cdaq/vcs2019/hallc_replay_vcs/%${savepwd}/%" set.csh
-sed -i -e "s%/home/cdaq/vcs2019/hallc_replay_vcs/%${savepwd}/%" run_full_replay.csh
-sed -i -e "s%/home/cdaq/vcs2019/hallc_replay_vcs/%${savepwd}/%" run_partial_replay.csh
-sed -i -e "s%/home/cdaq/vcs2019/hallc_replay_vcs/%${savepwd}/%" build_singlecoinfile.csh
-sed -i -e "s%/home/cdaq/vcs2019/hallc_replay_vcs/%${savepwd}/%" set.csh
-sed -i -e "s%/home/cdaq/vcs2019/hallc_replay_vcs/%${savepwd}/%" Ana/HallCana.cc
-sed -i -e "s%/home/cdaq/vcs2019/hallc_replay_vcs/%${savepwd}/%" Ana/includes/ReadHallCData.cc
-sed -i -e "s%/home/cdaq/vcs2019/hallc_replay_vcs/%${savepwd}/%" Ana/includes/ReadTextFiles.cc
 sed -i -e "s%/home/cdaq/vcs2019/hallc_replay_vcs/%${savepwd}/%" Ana/datainfo/get_runinfo.csh
 sed -i -e "s%/home/cdaq/vcs2019/hallc_replay_vcs/%${savepwd}/%" Ana/datainfo/get_mergeinfo.csh
 
